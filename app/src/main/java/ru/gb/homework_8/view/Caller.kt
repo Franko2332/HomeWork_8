c56bb851de4e9d3dd68e756b27503a1039d4b4ad
Franko777 <khubul.george@inbox.ru>
2022-04-08 16:27:51 +0300

    Added the ability to call a number from the contact list

---
+++ app/src/main/java/ru/gb/homework_8/view/Caller.kt
@@ -0,0 +1,5 @@
+package ru.gb.homework_8.view
+
+interface Caller {
+   public fun call(phoneNumber: String)
+}
--- app/src/main/java/ru/gb/homework_8/view/ContentProviderFragment.kt
+++ app/src/main/java/ru/gb/homework_8/view/ContentProviderFragment.kt
@@ -1,28 +1,37 @@
 package ru.gb.homework_8.view
 
 
-import android.os.Bundle
-
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
-import ru.gb.homework_8.databinding.FragmentContactsBinding
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
 import android.provider.ContactsContract
+import android.telecom.Call
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import ru.gb.homework_8.R
+import ru.gb.homework_8.databinding.ContactItemBinding
+import ru.gb.homework_8.databinding.FragmentContactsBinding
+import java.lang.Exception
 
 
-class ContentProviderFragment : Fragment() {
+class ContentProviderFragment : Fragment(), Caller {
+    private val _adapter: ContactsAdapter? = ContactsAdapter(this)
     private var _binding: FragmentContactsBinding? = null
     private val binding get() = _binding!!
     private val REQUEST_CODE = 42
+    private val REQUEST_CODE_FOR_CALLING = 24
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,8 +51,9 @@ class ContentProviderFragment : Fragment() {
     private fun checkPermission() {
         context?.let {
             when {
-                ContextCompat.checkSelfPermission(it, Manifest.permission.READ_CONTACTS) ==
+                ContextCompat.checkSelfPermission(it, Manifest.permission.READ_CONTACTS)  ==
                         PackageManager.PERMISSION_GRANTED -> getContacts()
+
 
                 shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) -> {
                     AlertDialog.Builder(it)
@@ -52,18 +62,39 @@ class ContentProviderFragment : Fragment() {
                             "В рамках учебного занятия на курсе" +
                                     " GB вам необоходимо предоставить доступ к контактам"
                         )
-                        .setPositiveButton("Предоставить", { _, _ -> requestPermission() })
-                        .setNegativeButton("Не надо", { dialog, _ -> dialog.dismiss() })
+                        .setPositiveButton("Предоставить", {
+                                _, _ -> requestPermission(REQUEST_CODE) })
+                        .setNegativeButton("Не надо", {
+                                dialog, _ -> dialog.dismiss() })
                         .create()
                         .show()
                 }
-                else -> requestPermission()
+                shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE) -> {
+                    AlertDialog.Builder(it)
+                        .setTitle("Доступ к вызовам")
+                        .setMessage(
+                            "В рамках учебного занятия на курсе" +
+                                    " GB вам необоходимо предоставить доступ к функции вызова"
+                        )
+                        .setPositiveButton("Предоставить", { _, _ ->
+                            requestPermission(REQUEST_CODE_FOR_CALLING)})
+                        .setNegativeButton("Не надо", { dialog, _ ->
+                            dialog.dismiss() })
+                        .create()
+                        .show()
+                }
+                else -> {
+                    requestPermission(REQUEST_CODE)
+                }
             }
         }
     }
 
-    private fun requestPermission() {
-        requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE)
+    private fun requestPermission(requestCode: Int) {
+        Log.e("req code", requestCode.toString())
+        requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS,
+                Manifest.permission.CALL_PHONE),
+                requestCode)
     }
 
     override fun onRequestPermissionsResult(
@@ -73,16 +104,18 @@ class ContentProviderFragment : Fragment() {
     ) {
         when (requestCode) {
             REQUEST_CODE -> {
-                if (grantResults.isNotEmpty()
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                if ((grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] ==
+                    PackageManager.PERMISSION_GRANTED) || (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 ) {
                     getContacts()
                 } else {
                     AlertDialog.Builder(requireContext())
                         .setTitle("Объяснение")
                         .setMessage(
-                            "В связи с непредоставлением доступа к контактам " +
-                                    "экран будет пустой"
+                            "Вы отказали в получении разрешений приложению , " +
+                                    "работа может быть нарушена"
                         )
                         .setNegativeButton("Закрыть", { dialog, _ -> dialog.dismiss() })
                         .create()
@@ -99,35 +132,38 @@ class ContentProviderFragment : Fragment() {
         requireContext()?.let {
             val contentResolver = it.contentResolver
             val cursorWithContact = contentResolver.query(
-                ContactsContract.Contacts.CONTENT_URI,
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                 null, null, null,
-                ContactsContract.Contacts.DISPLAY_NAME + " ASC"
+                null
             )
-            cursorWithContact?.let { cursor ->
-                for (i in 0..cursor.count) {
-                    if (cursor.moveToPosition(i)) {
-                        val name = cursor.getString(
-                            cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
-                        )
-                        addView(it, name)
-                    }
-                }
+            cursorWithContact?.let {
+                _adapter?.let { a -> a.setItems(it) }
             }
-            cursorWithContact?.close()
+            binding.contactsRecyclerView.apply {
+                this.adapter = _adapter
+                this.layoutManager = LinearLayoutManager(requireContext())
+            }
+
         }
 
     }
 
-    private fun addView(context: Context, name: String?) {
-        binding.containerForContacts.addView(TextView(context).apply {
-            text = name
-            textSize = resources.getDimension(R.dimen.text_size)
-        })
-    }
 
     companion object {
         @JvmStatic
         fun newInstance() = ContentProviderFragment()
+    }
+
+    override fun call(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_CALL)
+        intent.setData(Uri.parse("tel:"+phoneNumber))
+        try {
+            startActivity(intent)
+        }catch (ex: Exception) {
+            Toast.makeText(requireContext(),"Your call failed... " + ex.message,
+                Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
     }
 
 }
